64cb9114f3f29fa8044bed514308593d30088e87
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2025-09-25 14:33:38 +0600

    disabled contextmenu and shortcuts

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+import DisableInspect from "@/components/layout/DisableInspect";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <DisableInspect>{children}</DisableInspect>
         {children}
       </body>
     </html>
